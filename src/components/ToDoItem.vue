8e4c1b9d73f81f0564d965ff80b20c43726f16ba
charlie.wang <charlie.wang@alliance.tw>
2020-04-10 17:07:27 +0800

    feat: add todo feature

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <li class="container">
+    <p class="item-todo">{{ todoText }}</p>
+    <button class="button-delete" v-on:click="handleDelButtonClick(todoText)">X</button>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'ToDoItem',
+  props: {
+    id: Number,
+    todoText: String,
+    handleDelButtonClick: Function
+  },
+}
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 300px;
+  height: 60px;
+  margin: 0 auto 8px;
+  padding: 0 16px;
+  border: 1px solid #2c3e50;
+}
+
+.item-todo {
+  width: 262px;
+  height: 30px;
+  margin: 0;
+  text-align: initial;
+  line-height: 2;
+}
+
+.button-delete {
+  width: 24px;
+  height: 24px;
+  border: none;
+  border-radius: 6px;
+  color: #ffffff;
+  background: #d74153;
+  cursor: pointer;
+}
+</style>
--- src/components/VueToDo.vue
+++ src/components/VueToDo.vue
@@ -1,12 +1,71 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <div class="vue-to-do">
+    <h1>{{ msg }}</h1>
+    <input v-model="newTodo" class="input-add" placeholder="add todo.." />
+    <button class="button-add" v-on:click="handleAddButtonClick">Add</button>
+    <ul class="todo-item-section">
+      <ToDoItem v-for="todo in todoList" :key="todo.id" :todoText="todo.text" :handleDelButtonClick="handleDelButtonClick"/>
+    </ul>
+  </div>
 </template>
 
 <script>
+import ToDoItem from './ToDoItem'
+
 export default {
   name: 'VueToDo',
   props: {
     msg: String
+  },
+  components: {
+    ToDoItem
+  },
+  data: () => {
+    return {
+      newTodo: '',
+      count: 3,
+      todoList: [
+        {id: 0, text: 'Learning Vue'},
+        {id: 1, text: 'Lingoda Sprint'},
+        {id: 2, text: 'Final Fantasy VII Remake'}
+      ]
+    }
+  },
+  methods: {
+    handleAddButtonClick: function() {
+      this.todoList.push({id: this.count++ , text: this.newTodo})
+      this.newTodo = ''
+    },
+    handleDelButtonClick: function(todoText) {
+      const index = this.todoList.map(item => item.text).indexOf(todoText)
+      this.todoList.splice(index, 1)
+    }
   }
 }
 </script>
+
+<style scoped>
+.input-add {
+  width: 170px;
+  height: 30px;
+  padding: 0 8px;
+  margin-right: 8px;
+  box-sizing: border-box;
+  outline: none;
+}
+
+.button-add {
+  width: 72px;
+  height: 30px;
+  border: none;
+  border-radius: 6px;
+  background: #41b883;
+  cursor: pointer;
+}
+
+.todo-item-section {
+  margin-top: 36px;
+  padding: 0;
+}
+
+</style>
